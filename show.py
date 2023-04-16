9980f5da808876c42197a62c669d69e0d0e90b7f
Xia-ziling <130907592+Xia-ziling@users.noreply.github.com>
2023-04-16 21:15:43 +0800

    Delete show.py

--- show.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-conn = pymysql.connect(host='localhost', port=3306, user='root', password='123456', db='mydata_one')
-cursor = conn.cursor()
-sql = "select * from users"
-cursor.execute(sql)
-result = cursor.fetchall()
-for row in result:
-    print(row)
